09903110746b31bff73802267e6515db4276e363
Alison Hager <alisonhager@Alisons-Air.lan>
2024-02-21 16:50:38 -0500

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import Top from './Top'
+import Bottom from './Bottom'
+
+function Main() {
+
+  return (
+    <>
+    <Top />
+    <h2>Bottom</h2>
+    <Bottom />
+    </>
+  )
+}
+
+export default Main
